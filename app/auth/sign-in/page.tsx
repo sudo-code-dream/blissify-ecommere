854d03cb691a09d39cc3f9b2aa5b75e259fb0836
kennethdean <kennethjrdean99@gmail.com>
2025-06-16 20:56:51 +0800

    fix(auth): revert to using SignInForm in sign-in page

--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -2,10 +2,9 @@
 
 import React from "react";
 import SignInForm from "@/components/auth/SignInForm";
-import SignIn from "@/components/auth/page";
 
 const page = () => {
-  return <SignIn />;
+  return <SignInForm />;
 };
 
 export default page;
